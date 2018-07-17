1b39fef362f0b9f70bd221ffb682c85a84a72970
eileenwg <30696898+eileenwg@users.noreply.github.com>
2018-07-17 09:58:52 -0400

    update 17 juillet 2018

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,10 @@
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextField, FormField, FieldList
 from wtforms.validators import ValidationError, DataRequired
 from app.models import User
+
+
 
 
 class EditProfileForm(FlaskForm):
@@ -18,3 +20,43 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Veuillez utiliser un nom d\'utilisateur différent.')
+
+class InstallationPatient_dureeForm(FlaskForm):
+    class Meta:
+        csrf = False
+    Durée = TextField("Durée", validators=[DataRequired()])
+
+class sub_Injection(FlaskForm):
+    class Meta:
+        csrf = False
+    Contraste = TextField("Contraste", validators=[DataRequired()])
+    pré_scan = TextField("pré_scan", validators=[DataRequired()])
+    per_scan = TextField("per_scan", validators=[DataRequired()])
+
+class sub_Séquences(FlaskForm):
+    class Meta:
+        csrf = False
+    Nom = TextField("Nom", validators=[DataRequired()])
+    Durée = TextField("Durée", validators=[DataRequired()])
+
+
+class Production(FlaskForm):
+    class Meta:
+        csrf = False
+    Production = TextField("Production", validators=[DataRequired()])
+
+class EditProtocolsForm(FlaskForm):
+    Implantation = TextField("Implantation ",validators=[DataRequired()])
+    InstallationPatient = FormField(InstallationPatient_dureeForm)
+    Antenne = TextField("Antenne ",validators=[DataRequired()])
+    Séquences = FieldList(FormField(sub_Séquences))
+    Injection = FormField(sub_Injection)
+    Protocole_machine = TextField("Protocole_machine ",validators=[DataRequired()])
+    Durée_acquisition = TextField("Durée_acquisition ",validators=[DataRequired()])
+    Durée_examen = TextField("Durée_examen ",validators=[DataRequired()])
+    Durée_bloc_irm = TextField("Durée_bloc_irm ",validators=[DataRequired()])
+    Date_création = TextField("Date_création ",validators=[DataRequired()])
+    Date_dernière_modification = TextField("Date_dernière_modification ",validators=[DataRequired()])
+    Auteur = TextField("Auteur ",validators=[DataRequired()])
+    Statut = FormField(Production)
+    submit = SubmitField('Soumettre')
