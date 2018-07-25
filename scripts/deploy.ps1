c5a2c66ae1e8c1ba665e1a11b9bcb6b1464e49fa
Eleyine Zarour (CHUM) <eleyine@gmail.com>
2018-07-25 09:59:43 -0400

    update chum

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -2,5 +2,6 @@ docker kill protocols_container
 docker rm protocols_container
 
 docker build -t protocols_image ..
-docker run -d --name protocols_container -p 8008:8008 protocols_image
+
+docker run  -v c:/Users/p0001073/Protocoles/repo/container/data:/data -d --name protocols_container -p 8008:8008 protocols_image
 # docker exec -it protocols_container bash
